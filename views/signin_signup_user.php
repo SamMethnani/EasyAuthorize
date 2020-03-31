b45fc135656a37d76f010f5b52bbc6cfd404ef6a
SamMethnani <57623556+SamMethnani@users.noreply.github.com>
2020-03-31 12:02:35 +0100

    Update signin_signup_user.php

--- views/signin_signup_user.php
+++ views/signin_signup_user.php
@@ -62,7 +62,7 @@
                         </div>
                         <div class="form-group">
 
-                            <a href="#" class="ForgetPwd" value="Login">Forget Password?</a>
+                            <a href="#" class="ForgetPwd" value="Login">Forgot Password?</a>
                         </div>
                     </form>
                 </div>
